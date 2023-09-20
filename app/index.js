ef0b88ade83706c23c33f4cb11f4aaaf7d0d51e7
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-09-20 18:39:05 -0500

    formulario de login add

--- app/index.js
+++ app/index.js
@@ -1,12 +1,54 @@
-import { Text, View, Button } from "react-native";
 import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { Text, Button, TextInput, View, StyleSheet } from "react-native";
 
-export default function Login() {
+export default function Logueo() {
   const navigation = useRouter();
+  const [correo, setCorreo] = useState("");
+  const [contrasena, setContrasena] = useState("");
+
+  const enviarFormulario = () => {
+    navigation.push("home");
+  };
+
   return (
-    <View>
-      <Text>Ir a Home</Text>
-      <Button title="Ir" onPress={() => navigation.push("home")} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Logueate para ingresar </Text>
+      <TextInput
+        style={styles.input}
+        value={correo}
+        onChangeText={setCorreo}
+        placeholder="Ingresa tu correo"
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        value={contrasena}
+        onChangeText={setContrasena}
+        placeholder="Ingresa tu contraseña"
+        secureTextEntry
+      />
+      <Button title="Enviar" onPress={enviarFormulario} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingLeft: 8,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 34,
+    textAlign: "center",
+  },
+});
